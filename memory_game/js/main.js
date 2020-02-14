54a3ed6f259c777006ef664f46f61497f580b40c
ajay-k <ajay_k@sbcglobal.net>
2020-02-14 12:08:24 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,13 @@
+//Declare 4 variables for each card
+let cardOne, cardTwo, cardThree, cardFour;
+
+//Set two card values to queen
+cardOne = "queen";
+cardTwo = "queen";
+
+//Set two card values to king
+cardThree = "king";
+cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
