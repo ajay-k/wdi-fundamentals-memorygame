99387c503b466ef1adb00c473430ed6fc80e52b3
ajay-k <ajay_k@sbcglobal.net>
2020-02-15 01:10:28 -0800

    created game logic/Finished Control Flow

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,22 @@
-//Declare 4 variables for each card
-let cardOne, cardTwo, cardThree, cardFour;
+//Declare array to store cards
+const cards = ["queen", "queen", "king", "king"];
 
-//Set two card values to queen
-cardOne = "queen";
-cardTwo = "queen";
+//Declare array to hold cards in play
+const cardsInPlay = [];
 
-//Set two card values to king
-cardThree = "king";
-cardFour = "king";
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped queen");
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again");
+	}
+}
