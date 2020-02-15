48cd310e4e6513b631e3b708208ee94d8c48b104
ajay-k <ajay_k@sbcglobal.net>
2020-02-15 15:29:57 -0800

    created game logic/Javascript Functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,19 +4,26 @@ const cards = ["queen", "queen", "king", "king"];
 //Declare array to hold cards in play
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped queen");
-
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}else{
-		alert("Sorry, try again");
+function checkForMatch(){
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			console.log("You found a match!");
+		}else{
+			console.log("Sorry, try again");
+		}
 	}
 }
+
+function flipCard(cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+
+	//Add the card the user flipped over to cardsInPlay array
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+
+}
+
+flipCard(0);
+flipCard(2);
+
