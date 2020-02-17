9ca6f733ddc434e3668f0262844069424f0ec882
ajay-k <ajay_k@sbcglobal.net>
2020-02-16 16:36:29 -0800

    Finished Memory Game/DOM

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,27 +27,41 @@ const cards = [
 const cardsInPlay = [];
 
 function checkForMatch(){
-	if(cardsInPlay.length === 2){
-		if(cardsInPlay[0] === cardsInPlay[1]){
-			console.log("You found a match!");
-		}else{
-			console.log("Sorry, try again");
-		}
+	
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again");
 	}
 }
 
-function flipCard(cardId) {
-
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
+	console.log(cardId.value);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("Image: " + cards[cardId].cardImage)
+
 	//Add the card the user flipped over to cardsInPlay array
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	this.setAttribute('src',cards[cardId].cardImage);
+	//If user selected two cards check if they match
+	if(cardsInPlay.length == 2){
+		checkForMatch();
+	}
 
 
 
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for(let i = 0; i < cards.length; i++){
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
 
